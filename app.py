1544d6cb4d6ddb4fd9ef475474d60c6aa5779ff7
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-03-26 11:46:23 -0300

    creamos dos rutas con links

--- app.py
+++ app.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/")  #devuelve lo q ve en el navegador
+def principal():
+    return """  
+        <a href='/buenos dias'>buenos dias</a>
+        <a href='/buenas noches'>buenas noches</a>
+    """
+
+@app.route("/buenos dias") #si se presiona hola se muestra holanda
+def hello_world(): 
+    return "<p>como amaneciste!</p>"
+
+@app.route("/buenas noches") #pero si se presiona chau muestra chau
+def despedir():
+    return "<p>descansa</p>"
+#dos rutas y dos links
