be480cd936f4e8e8ec20ed32bf09f93793a970cb
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-04-09 12:03:56 -0300

    agregamos url_for y comando para correr app con el depurador

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route("/")  #devuelve lo q ve en el navegador
 def principal():
+    url_hola = url_for("hello_world")
+    url_dado = url_for("dado", caras=6)
+    url_logo = url_for("static", filename="img/badbo.jpeg")
     return """  
         <a href='/buenos dias'>buenos dias</a>
         <a href='/buenas noches'>buenas noches</a>
+         <a href="{url_hola}">Hola</a>
+        <br>
+        <a href="{url_for("despedir")}">Chau</a>
+         <br>
+        <a href="{url_logo}">Logo</a> 
+        <br>
+        <a href="{url_dado}">Tirar_dado</a>
     """
 
 @app.route("/buenos dias") #si se presiona hola se muestra holanda
@@ -21,6 +31,22 @@ def despedir():
 @app.route("/hola")
 def saludar():
     return "<h2>Hola!</h2>"
+
 @app.route("/hola/<string:nombre>") #el argumento nombre, se pasa por parametro en la ruta.
 def saludar_con_nombre (nombre):
     return f"<h2>Hola {nombre}!</h2>"
+
+def main():
+    url_hola = url_for("hello_world") #es una variable, llama lo que esta en el def
+    url_dado = url_for("dado", caras=6)
+    url_logo = url_for("static", filename="img/badbo.jpeg")
+
+    return f"""
+    <a href="{url_hola}">Hola</a> #NN not null, sin valor
+    <br>
+    <a href="{url_for("despedir")}">Chau</a>
+    <br>
+    <a href="{url_logo}">Logo</a> 
+    <br>
+    <a href="{url_dado}">Tirar_dado</a>
+    """
