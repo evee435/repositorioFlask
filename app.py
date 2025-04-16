e527c7b35904b8e0bb5213fccf9f8c1f4af4f7f7
evelyn villarreal <evelyn.villarreal245@gmail.com>
2025-04-16 11:52:04 -0300

    conexion a base de datos

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
 from flask import Flask, url_for
-
+import sqlite3
 app = Flask(__name__)
+db=None
 
-@app.route("/")  #devuelve lo q ve en el navegador
+#------------------CONEXION A BASE DE DATOS
+
+def abrirConexion():
+    global db
+    db = sqlite3.connect('instance/datos.sqlite')
+    db.row_factory = sqlite3.Row
+    return db
+
+def cerrarConexion(): 
+    global db
+    if db is not None:
+        db.close()
+        db=None
+
+@app.route("/usuarios/")
+def obtenerGente():
+    global db
+    conexion = abrirConexion()
+    cursor = conexion.cursor()
+    cursor.execute('SELECT * FROM usuarios')
+    resultado = cursor.fetchall()
+    cerrarConexion()
+    fila = [dict(row) for row in resultado]
+    return str(fila)
+
+
+#-----------------------------------------------------------------------------------------------------
+
+
+@app.route("/")  #devuelve lo q ve en el navegador, ruta
 def principal():
     url_hola = url_for("hello_world")
-    url_dado = url_for("dado", caras=6)
+    #url_dado = url_for("dado", caras=6)
     url_logo = url_for("static", filename="img/badbo.jpeg")
     return """  
         <a href='/buenos dias'>buenos dias</a>
@@ -23,7 +53,7 @@ def principal():
 def hello_world(): 
     return "<p>como amaneciste!</p>"
 
-@app.route("/buenas noches") #pero si se presiona chau muestra chau
+@app.route("/buenas noches") #hello_worldpero si se presiona chau muestra chau
 def despedir():
     return "<p>descansa</p>"
 #dos rutas y dos links
@@ -31,6 +61,7 @@ def despedir():
 @app.route("/hola")
 def saludar():
     return "<h2>Hola!</h2>"
+
 
 @app.route("/hola/<string:nombre>") #el argumento nombre, se pasa por parametro en la ruta.
 def saludar_con_nombre (nombre):
